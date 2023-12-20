8d95b0fc18b98329fc013b99a117a1e6a6076f7d
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2023-12-21 00:45:42 +0200

    Design of Immediate Sign Extend Unit

--- design/immext.sv
+++ design/immext.sv
@@ -1,15 +1,19 @@
 module riscv_extend (i_riscv_extend_inst, i_riscv_extend_immsrc, o_riscv_extend_simm);
 
-  typedef enum {i_imm, u_imm, j_imm, s_imm, b_imm} imm_type;
+  typedef enum logic [2:0] {i_imm = 3'b000,
+                            u_imm = 3'b001,
+                            j_imm = 3'b010,
+                            s_imm = 3'b011,
+                            b_imm = 3'b100} imm_type;
 
+  input   logic   [2:0]   i_riscv_extend_immsrc;
   input   logic   [31:7]  i_riscv_extend_inst;
   output  logic   [63:0]  o_riscv_extend_simm;
-  input imm_type  i_riscv_extend_immsrc;
-  
 
+  imm_type immsrc;
 
   always_comb begin
-    case(i_riscv_extend_immsrc)
+    case(immsrc)
       i_imm: o_riscv_extend_simm = { {53{i_riscv_extend_inst[31]}}, i_riscv_extend_inst[30:20] };
       u_imm: o_riscv_extend_simm = { {33{i_riscv_extend_inst[31]}}, i_riscv_extend_inst[30:12], {12{1'b0}} };
       j_imm: o_riscv_extend_simm = { {44{i_riscv_extend_inst[31]}}, i_riscv_extend_inst[19:12], i_riscv_extend_inst[20], i_riscv_extend_inst[30:21], 1'b0 };
@@ -18,5 +22,7 @@ module riscv_extend (i_riscv_extend_inst, i_riscv_extend_immsrc, o_riscv_extend_
       default: o_riscv_extend_simm = 'bx;
     endcase
   end
+
+  assign immsrc = imm_type'(i_riscv_extend_immsrc);
 
 endmodule : riscv_extend 
