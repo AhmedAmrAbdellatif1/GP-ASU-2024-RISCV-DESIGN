451d2d9412273b053d713b93380e67530f50d2f4
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2023-12-22 03:38:12 +0200

    Update control_unit_tb.sv
    Testing Control Unit is DONE

--- testbench/control_unit_tb.sv
+++ testbench/control_unit_tb.sv
@@ -3,7 +3,7 @@
 /* Last Modified Date: 12/22/2023                         */
 /* By: Ahmed Amr Abdellatif                               */
 /**********************************************************/
-`timescale 1ns/1ns;
+`timescale 1ns/1ns
 
 module riscv_cu_tb();
 /*********************** Parameters ***********************/
@@ -34,51 +34,30 @@ module riscv_cu_tb();
 /********************* Initial Blocks *********************/
   initial begin : proc_testing
     $readmemh("cu_testcases.txt", instruction);
-    
+     
     for(i=0;i<TESTCASES;i++) begin
       opcode    = instruction[i][6:0];
       funct3    = instruction[i][14:12];
       funct7_5  = instruction[i][30];
       #DELAY
-      if((opcode == 'd51) || (opcode == 'd59)) begin
-        r_type_test();
-        #DELAY;
-      end
+      case(opcode)
+        'd51:   r_type_test();
+        'd59:   r_type_test();
+        'd19:   imm_type_test();
+        'd27:   imm_type_test();
+        'd03:   load_test();
+        'd103:  jump_test();
+        'd111:  jump_test();
+        'd55:   lui_test();
+        'd23:   auipc_test();
+        'd35:   store_test();
+        'd99:   branch_test();
+      endcase
+      #DELAY;
     end
   end
   
 /******************** Tasks & Functions *******************/
-task control_signals_test;
-  input [2:0] immsrc_test;
-  input       regw_test;
-  input       asel_test;
-  input       bsel_test;
-  input [4:0] aluctrl_test;
-  input [1:0] storesrc_test;
-  input [2:0] bcond_test;
-  input       memw_test;
-  input [2:0] memext_test;
-  input [1:0] resultsrc_test;
-  input       jump_test;
-  begin
-    if( (immsrc     != immsrc_test)     ||
-        (regw       != regw_test)       ||
-        (asel       != asel_test)       ||
-        (bsel       != bsel_test)       ||
-        (aluctrl    != aluctrl_test)    ||
-        (storesrc   != storesrc_test)   ||
-        (bcond      != bcond_test)      ||
-        (memw       != memw_test)       ||
-        (memext     != memext_test)     ||
-        (resultsrc  != resultsrc_test)  ||
-        (jump       !=  jump_test)      
-      ) begin
-        $display("instruction(#%2d): There's an error",(i+1));
-      end else begin
-        $display("instruction(#%2d): Passed",(i+1));
-      end
-  end
-endtask
 
 task r_type_test();
   begin
@@ -280,6 +259,313 @@ task r_type_test();
   end
 endtask
 
+task imm_type_test();
+  begin
+    case(funct3)
+    3'b000: begin
+      if(opcode == 7'd19) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b00000) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end else if(opcode == 7'd27) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b10000) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end
+    end
+    3'b001: begin
+      if((funct7_5 == 1'b0) && (opcode[3] == 1'b0)) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b00010) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end else if((funct7_5 == 1'b0) && (opcode[3] == 1'b1)) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b10010) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end      
+    end
+    3'b010: begin
+      if( (immsrc == 3'b000) &&
+          (regw == 1'b1) &&
+          (asel == 1'b1) &&
+          (bsel == 1'b1) &&
+          (aluctrl == 5'b00011) &&
+          (bcond == 4'b0000) &&
+          (memw == 1'b0) &&
+          (resultsrc == 2'b01) &&
+          (jump == 1'b0) )
+            $display("Instruction(#%2d): Passed",(i+1));
+          else
+            $display("Instruction(#%2d): Failed",(i+1));
+    end
+    3'b011: begin
+      if( (immsrc == 3'b000) &&
+          (regw == 1'b1) &&
+          (asel == 1'b1) &&
+          (bsel == 1'b1) &&
+          (aluctrl == 5'b00100) &&
+          (bcond == 4'b0000) &&
+          (memw == 1'b0) &&
+          (resultsrc == 2'b01) &&
+          (jump == 1'b0) )
+            $display("Instruction(#%2d): Passed",(i+1));
+          else
+            $display("Instruction(#%2d): Failed",(i+1));
+    end
+    3'b100: begin
+      if( (immsrc == 3'b000) &&
+          (regw == 1'b1) &&
+          (asel == 1'b1) &&
+          (bsel == 1'b1) &&
+          (aluctrl == 5'b00101) &&
+          (bcond == 4'b0000) &&
+          (memw == 1'b0) &&
+          (resultsrc == 2'b01) &&
+          (jump == 1'b0) )
+            $display("Instruction(#%2d): Passed",(i+1));
+          else
+            $display("Instruction(#%2d): Failed",(i+1));
+    end
+    3'b101: begin
+      if((funct7_5 == 1'b0) && (opcode[3] == 1'b0)) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b00110) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end else if((funct7_5 == 1'b1) && (opcode[3] == 1'b0)) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b00111) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end else if((funct7_5 == 1'b0) && (opcode[3] == 1'b1)) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b10101) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end else if((funct7_5 == 1'b1) && (opcode[3] == 1'b1)) begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b10110) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b0) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end      
+    end
+    3'b110: begin
+      if( (immsrc == 3'b000) &&
+          (regw == 1'b1) &&
+          (asel == 1'b1) &&
+          (bsel == 1'b1) &&
+          (aluctrl == 5'b01000) &&
+          (bcond == 4'b0000) &&
+          (memw == 1'b0) &&
+          (resultsrc == 2'b01) &&
+          (jump == 1'b0) )
+            $display("Instruction(#%2d): Passed",(i+1));
+          else
+            $display("Instruction(#%2d): Failed",(i+1));
+    end
+    3'b111: begin
+      if( (immsrc == 3'b000) &&
+          (regw == 1'b1) &&
+          (asel == 1'b1) &&
+          (bsel == 1'b1) &&
+          (aluctrl == 5'b01001) &&
+          (bcond == 4'b0000) &&
+          (memw == 1'b0) &&
+          (resultsrc == 2'b01) &&
+          (jump == 1'b0) )
+            $display("Instruction(#%2d): Passed",(i+1));
+          else
+            $display("Instruction(#%2d): Failed",(i+1));
+    end
+    endcase
+  end
+endtask
+
+task load_test();
+  begin
+    if( (immsrc == 3'b000) &&
+        (regw == 1'b1) &&
+        (asel == 1'b1) &&
+        (bsel == 1'b1) &&
+        (aluctrl == 5'b00000) &&
+        (bcond == 4'b0000) &&
+        (memw == 1'b0) &&
+        (memext == funct3) &&
+        (resultsrc == 2'b10) &&
+        (jump == 1'b0) )
+          $display("Instruction(#%2d): Passed",(i+1));
+    else
+          $display("Instruction(#%2d): Failed",(i+1));
+  end
+endtask
+
+task jump_test();
+  begin
+    case(opcode)
+      'd103: begin
+        if( (immsrc == 3'b000) &&
+            (regw == 1'b1) &&
+            (asel == 1'b1) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b01010) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b00) &&
+            (jump == 1'b1) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end
+      'd111: begin
+        if( (immsrc == 3'b010) &&
+            (regw == 1'b1) &&
+            (asel == 1'b0) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b00000) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b00) &&
+            (jump == 1'b1) )
+              $display("Instruction(#%2d): Passed",(i+1));
+            else
+              $display("Instruction(#%2d): Failed",(i+1));
+      end
+    endcase
+  end
+endtask
+
+task lui_test();
+  begin
+    if( (immsrc == 3'b001) &&
+        (regw == 1'b1) &&
+        (bcond == 4'b0000) &&
+        (memw == 1'b0) &&
+        (resultsrc == 2'b11) &&
+        (jump == 1'b0) )
+          $display("Instruction(#%2d): Passed",(i+1));
+    else
+          $display("Instruction(#%2d): Failed",(i+1));
+  end
+endtask
+
+task auipc_test();
+  begin
+    if( (immsrc == 3'b001) &&
+            (regw == 1'b1) &&
+            (asel == 1'b0) &&
+            (bsel == 1'b1) &&
+            (aluctrl == 5'b00000) &&
+            (bcond == 4'b0000) &&
+            (memw == 1'b0) &&
+            (resultsrc == 2'b01) &&
+            (jump == 1'b1) )
+              $display("Instruction(#%2d): Passed",(i+1));
+    else
+              $display("Instruction(#%2d): Failed",(i+1));
+  end
+endtask
+
+task store_test();
+  begin
+    if( (immsrc == 3'b011) &&
+        (regw == 1'b0) &&
+        (asel == 1'b1) &&
+        (bsel == 1'b1) &&
+        (aluctrl == 5'b00000) &&
+        (storesrc == funct3[1:0]) &&
+        (bcond == 4'b0000) &&
+        (memw == 1'b1) &&
+        (jump == 1'b0) )
+          $display("Instruction(#%2d): Passed",(i+1));
+    else
+          $display("Instruction(#%2d): Failed",(i+1));
+  end
+endtask
+
+task branch_test();
+  begin
+    if( (immsrc == 3'b100) &&
+          (regw == 1'b0) &&
+          (asel == 1'b0) &&
+          (bsel == 1'b1) &&
+          (aluctrl == 5'b00000) &&
+          (bcond[3] == 1'b1) &&
+          (bcond[2:0] == funct3) &&
+          (memw == 1'b1) &&
+          (jump == 1'b0) )
+            $display("Instruction(#%2d): Passed",(i+1));
+      else
+            $display("Instruction(#%2d): Failed",(i+1));
+  end
+endtask
 
 /******************** DUT Instantiation *******************/
 
