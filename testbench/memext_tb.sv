6e9cc6c54edbcf6ad3f0ad8b7a0901c102410306
Abdelrahman1900799 <105523284+Abdelrahman1900799@users.noreply.github.com>
2023-12-22 04:46:36 +0200

    RF - PPreg-memEXT (Until now)

---
+++ testbench/memext_tb.sv
@@ -0,0 +1,121 @@
+/**********************************************************/
+/* Module Name:                                           */
+/* Last Modified Date:                                    */
+/* By:   Abdelarhaman                                     */
+/**********************************************************/
+`timescale 1ns/1ns;
+
+module riscv_memext_tb();
+
+/*********************** Parameters ***********************/
+  parameter DELAY =50;
+  parameter TEST_NUM = 100;
+  parameter BYTE_SIZE =8;
+  parameter HALF_WORD_SIZE= 16;
+  parameter WORD_SIZE =32;
+  parameter DOUBLE_WORD_SIZE = 64;
+/************** Internal Signals Declaration **************/
+  logic  [2:0]  sel;
+  logic  [63:0] data;
+  logic  [63:0] out;
+  logic  [63:0] data_random [99:0];
+  integer i;
+/********************* Initial Blocks *********************/
+  initial begin : proc_testing
+    $readmemh("100_random_64bits",data_random);
+    
+    //Load Byte 
+    sel = 000; //signed load byte
+    Signed_all_inputs_posibiltes_BYTE();
+    #DELAY
+    sel = 100;
+    Unsigned_all_inputs_posibiltes("Load Byte",data,BYTE_SIZE);
+    #DELAY
+    //Half word
+    sel = 001;
+    Signed_all_inputs_posibiltes_HALFWORD();
+    #DELAY
+    sel = 101;
+    Unsigned_all_inputs_posibiltes("Half word",data,HALF_WORD_SIZE);
+    #DELAY
+    //Word
+    sel = 010;
+    Signed_random_inputs_Word();
+    #DELAY
+    sel = 010;
+    Unsigned_random_inputs("Word",data,WORD_SIZE);
+    // Double word
+    sel = 011;
+    Unsigned_random_inputs("Word",data,DOUBLE_WORD_SIZE);
+       
+    
+  end
+  
+  
+
+
+/******************** Tasks & Functions *******************/
+task Signed_all_inputs_posibiltes_BYTE();
+    for(i=0;i<2^BYTE_SIZE;i=i+1)
+      begin
+      data = i; 
+      #DELAY
+      if(out !== {{56{data[7]}},{data[7:0]}})
+        $display("BYTE %b error_signed out = %b",data,out);
+     end
+  
+endtask
+task Signed_all_inputs_posibiltes_HALFWORD();
+    for(i=0;i<2^HALF_WORD_SIZE;i=i+1)
+      begin
+      data = i; 
+      #DELAY
+      if(out !== {{48{data[15]}},{data[15-1:0]}})
+        $display("Half WORD %b error_signed out = %b",data,out);
+     end
+  
+endtask
+
+task Unsigned_all_inputs_posibiltes(string TYPE, logic [63:0] data , [6:0] Size);
+    for(i=0;i<2^Size;i=i+1)
+      begin
+      data = i; 
+      #DELAY
+      if(out !== data)
+        $display("%s %b error_unsigned out = %b",TYPE,data,out);
+     end
+  
+endtask
+
+task Signed_random_inputs_Word();
+    for(i=0;i<TEST_NUM;i=i+1)
+      begin
+      data = data_random[i]; 
+      #DELAY
+      if(out !== {{32{data[31]}},{data[31:0]}})
+        $display("Word %b error_signed out = %b",data,out);
+     end
+  
+endtask
+task Unsigned_random_inputs(string TYPE, logic [63:0] data , [6:0] Size);
+    for(i=0;i<TEST_NUM;i=i+1)
+      begin
+      data = data_random[i]; 
+      #DELAY
+      if(out !== data)
+        $display("%s %b error_unsigned out = %b",TYPE,data,out);
+     end
+  
+endtask
+
+
+/******************** DUT Instantiation *******************/
+
+  riscv_memext DUT
+  (
+    .i_riscv_memext_sel(sel),
+    .i_riscv_memext_data(data),
+    .o_riscv_memext_loaded(out)
+  );
+endmodule
+
--- testbench/rf_tb.sv
+++ testbench/rf_tb.sv
@@ -1,1 +1,117 @@
-t
+`timescale 1ns/1ps
+
+module riscv_rf_tb();
+ 
+ 
+/*********************** Parameters ***********************/
+  parameter CLK_PERIOD = 50;
+  parameter HALF_PERIOD = CLK_PERIOD/2;
+  parameter WIDTH = 64;
+  parameter DEPTH = 32;
+  parameter ADDR = 5 ;
+
+/************** Internal Signals Declaration **************/
+logic              clk_n;
+logic              regwrite;
+logic  [ADDR-1:0]  rs1addr;
+logic  [ADDR-1:0]  rs2addr;
+logic  [WIDTH-1:0] rddata;
+logic  [ADDR-1:0]  rdaddr;
+logic  [WIDTH-1:0] rs1data;
+logic  [WIDTH-1:0] rs2data; 
+integer       i;
+
+  /** Clock Generation Block **/
+  initial begin : proc_clock
+    clk_n = 1'b1;
+    forever begin
+      #HALF_PERIOD clk_n = ~clk_n;
+    end
+  end
+initial
+begin
+  // writing checking
+regwrite='b1;
+rddata= 'b111;
+rdaddr='b101;
+
+#CLK_PERIOD
+for(i=0;i<DEPTH;i=i+1) // check that only the address 101 in written in it
+  begin
+    $display("Register %d: %d",i,dut.rf[i]); 
+  end
+      CheckEquality("Register", rddata, dut.rf[rdaddr]);
+      
+#CLK_PERIOD
+for(i=0;i<DEPTH;i=i+1) // check that every address can be written in
+  begin
+    rdaddr = i;
+    rddata= i;
+    #CLK_PERIOD
+    $display("Register %d: %d",i,dut.rf[i]); 
+    CheckEquality("Register", rddata, dut.rf[rdaddr]);
+  end
+    #CLK_PERIOD
+    rdaddr = 0;  // check that Register 0 cant be written in
+    rddata= 'b10101010;
+    #CLK_PERIOD
+    CheckEquality("Register 0", 0, dut.rf[0]);
+    $display("Register 0 %d",dut.rf[0]); 
+    
+    //Reading Checking
+    rs2addr = 'b101;
+    rs1addr = 'b1111;
+    regwrite = 'b1;
+    #CLK_PERIOD
+for(i=0;i<DEPTH;i=i+1) // check that every rs1 address can be reads succsfully
+  begin
+    rs1addr = i;
+    #CLK_PERIOD
+    $display("Register %d: %d",i,dut.rf[i]); 
+    CheckEquality("Register", rs1data, dut.rf[rs1addr]);    
+  end 
+      #CLK_PERIOD
+for(i=0;i<DEPTH;i=i+1) // check that every rs2 address can be reads succsfully
+  begin
+    rs2addr = i;
+    #CLK_PERIOD
+    $display("Register %d: %d",i,dut.rf[i]); 
+    CheckEquality("Read Register", rs2data, dut.rf[rs2addr]);    
+  end 
+  
+  // checking reading register zero 
+  rs2addr = 0;
+  rs1addr = 0;
+  #CLK_PERIOD
+  CheckEquality("Read Rs1 Register 0", rs1data, 0);
+  CheckEquality("Read Rs2 Register 0", rs2data, 0);
+  
+#100  
+$stop;
+ end
+
+
+/******************** Tasks & Functions *******************/
+task CheckEquality(string signal_name, logic [WIDTH-1:0] A, logic [WIDTH-1:0] B);
+    if (A === B)
+      begin
+      $display("%s Success", signal_name);
+      end
+    else
+      begin
+      $display("%s Failure", signal_name);
+      end
+endtask
+
+riscv_rf dut( 
+    .i_riscv_rf_clk_n(clk_n),
+    .i_riscv_rf_regwrite(regwrite),
+    .i_riscv_rf_rs1addr(rs1addr),
+    .i_riscv_rf_rs2addr(rs2addr),
+    .o_riscv_rf_rs1data(rs1data),
+    .o_riscv_rf_rs2data(rs2data),
+    .i_riscv_rf_rddata(rddata),
+    .i_riscv_rf_rdaddr(rdaddr)
+); 
+endmodule
+
