f68b8959bcd43ab225ab389c9669650c8e61c270
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2023-12-23 11:55:43 +0200

    Update branch_comp_tb.sv

--- testbench/branch_comp_tb.sv
+++ testbench/branch_comp_tb.sv
@@ -20,14 +20,214 @@ module riscv_branch_tb()
 
 /********************* Initial Blocks *********************/
   initial begin : proc_testing
-    
+     bcond[3] = 1'b1;
+     
+     // (1) Branch if equal test
+     bcond[2:0] = BEQ;
+      // (1.1) rs1 > rs2
+      rs1 = 'sd3060;
+      rs2 = 'sd2965;
+      #DELAY;
+      if(branch != 1'b0) $display("[BEQ]  Testcase (1): Failed");
+      else               $display("[BEQ]  Testcase (1): Passed");
+      // (1.2) rs1 = rs2
+      rs1 = 'sd2044;
+      rs2 = 'sd2044;
+      #DELAY;
+      if(branch != 1'b1) $display("[BEQ]  Testcase (2): Failed");
+      else               $display("[BEQ]  Testcase (2): Passed");
+      // (1.3) rs1 = -rs2
+      rs1 = 'sd1505;
+      rs2 =-'sd1505;
+      #DELAY;
+      if(branch != 1'b0) $display("[BEQ]  Testcase (3): Failed");
+      else               $display("[BEQ]  Testcase (3): Passed");
+
+
+      // (2) Branch if not equal test
+     bcond[2:0] = BNE;
+      // (2.1) rs1 > rs2
+      rs1 = 'sd3060;
+      rs2 = 'sd2965;
+      #DELAY;
+      if(branch != 1'b1) $display("[BNE]  Testcase (1): Failed");
+      else               $display("[BNE]  Testcase (1): Passed");
+      // (2.2) rs1 = rs2
+      rs1 = 'sd2044;
+      rs2 = 'sd2044;
+      #DELAY;
+      if(branch != 1'b0) $display("[BNE]  Testcase (2): Failed");
+      else               $display("[BNE]  Testcase (2): Passed");
+      // (2.3) rs1 = -rs2
+      rs1 = 'sd1505;
+      rs2 =-'sd1505;
+      #DELAY;
+      if(branch != 1'b1) $display("[BNE]  Testcase (3): Failed");
+      else               $display("[BNE]  Testcase (3): Passed");
+
+
+      // (3) Branch if less than test
+     bcond[2:0] = BLT;
+      // (3.1) two possitive numbers
+      rs1 = 'sd3060;
+      rs2 = 'sd2965;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLT]  Testcase (1): Failed");
+      else               $display("[BLT]  Testcase (1): Passed");
+      // (3.2) two possitive negative numbers
+      rs1 =-'sd3060;
+      rs2 =-'sd2965;
+      #DELAY;
+      if(branch != 1'b1) $display("[BLT]  Testcase (2): Failed");
+      else               $display("[BLT]  Testcase (2): Passed");
+      // (3.3) one positive one negative
+      rs1 = 'sd4124;
+      rs2 =-'sd5688;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLT]  Testcase (3): Failed");
+      else               $display("[BLT]  Testcase (3): Passed");
+      // (3.4) one negative one positive
+      rs1 =-'sd4124;
+      rs2 = 'sd5688;
+      #DELAY;
+      if(branch != 1'b1) $display("[BLT]  Testcase (4): Failed");
+      else               $display("[BLT]  Testcase (4): Passed");
+      // (3.5) two equal numbers
+      rs1 = 'sd12457;
+      rs2 = 'sd12457;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLT]  Testcase (5): Failed");
+      else               $display("[BLT]  Testcase (5): Passed");
+      // (3.6) not equal numbers
+      rs1 = 'sd12457;
+      rs2 =-'sd12457;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLT]  Testcase (6): Failed");
+      else               $display("[BLT]  Testcase (6): Passed");
+
+
+      // (4) Branch if greater than or equal test
+     bcond[2:0] = BLT;
+      // (4.1) two possitive numbers
+      rs1 = 'sd3060;
+      rs2 = 'sd2965;
+      #DELAY;
+      if(branch != 1'b1) $display("[BGE]  Testcase (1): Failed");
+      else               $display("[BGE]  Testcase (1): Passed");
+      // (4.2) two possitive negative numbers
+      rs1 =-'sd3060;
+      rs2 =-'sd2965;
+      #DELAY;
+      if(branch != 1'b0) $display("[BGE]  Testcase (2): Failed");
+      else               $display("[BGE]  Testcase (2): Passed");
+      // (4.3) one positive one negative
+      rs1 = 'sd4124;
+      rs2 =-'sd5688;
+      #DELAY;
+      if(branch != 1'b1) $display("[BGE]  Testcase (3): Failed");
+      else               $display("[BGE]  Testcase (3): Passed");
+      // (4.4) one negative one positive
+      rs1 =-'sd4124;
+      rs2 = 'sd5688;
+      #DELAY;
+      if(branch != 1'b0) $display("[BGE]  Testcase (4): Failed");
+      else               $display("[BGE]  Testcase (4): Passed");
+      // (4.5) two equal numbers
+      rs1 = 'sd12457;
+      rs2 = 'sd12457;
+      #DELAY;
+      if(branch != 1'b1) $display("[BGE]  Testcase (5): Failed");
+      else               $display("[BGE]  Testcase (5): Passed");
+      // (4.6) not equal numbers
+      rs1 =-'sd12457;
+      rs2 = 'sd12457;
+      #DELAY;
+      if(branch != 1'b0) $display("[BGE]  Testcase (6): Failed");
+      else               $display("[BGE]  Testcase (6): Passed");
+
+
+      // (5) Branch if less than unsigned test
+     bcond[2:0] = BLTU;
+      // (5.1) two possitive numbers
+      rs1 = 'sd3060;
+      rs2 = 'sd2965;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLTU] Testcase (1): Failed");
+      else               $display("[BLTU] Testcase (1): Passed");
+      // (5.2) two possitive negative numbers
+      rs1 =-'sd3060;
+      rs2 =-'sd2965;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLTU] Testcase (2): Failed");
+      else               $display("[BLTU] Testcase (2): Passed");
+      // (5.3) one positive one negative
+      rs1 = 'sd4124;
+      rs2 =-'sd5688;
+      #DELAY;
+      if(branch != 1'b1) $display("[BLTU] Testcase (3): Failed");
+      else               $display("[BLTU] Testcase (3): Passed");
+      // (5.4) one negative one positive
+      rs1 =-'sd4124;
+      rs2 = 'sd5688;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLTU] Testcase (4): Failed");
+      else               $display("[BLTU] Testcase (4): Passed");
+      // (5.5) two equal numbers
+      rs1 = 'sd12457;
+      rs2 = 'sd12457;
+      #DELAY;
+      if(branch != 1'b0) $display("[BLTU] Testcase (5): Failed");
+      else               $display("[BLTU] Testcase (5): Passed");
+      // (5.6) not equal numbers
+      rs1 = 'sd12457;
+      rs2 =-'sd12457;
+      #DELAY;
+      if(branch != 1'b1) $display("[BLTU] Testcase (6): Failed");
+      else               $display("[BLTU] Testcase (6): Passed");
+
+
+      // (6) Branch if greater than or equal test
+     bcond[2:0] = BGEU;
+      // (6.1) two possitive numbers
+      rs1 = 'sd3060;
+      rs2 = 'sd2965;
+      #DELAY;
+      if(branch != 1'b1) $display("[BGEU] Testcase (1): Failed");
+      else               $display("[BGEU] Testcase (1): Passed");
+      // (6.2) two possitive negative numbers
+      rs1 =-'sd3060;
+      rs2 =-'sd2965;
+      #DELAY;
+      if(branch != 1'b1) $display("[BGEU] Testcase (2): Failed");
+      else               $display("[BGEU] Testcase (2): Passed");
+      // (6.3) one positive one negative
+      rs1 = 'sd4124;
+      rs2 =-'sd5688;
+      #DELAY;
+      if(branch != 1'b0) $display("[BGEU] Testcase (3): Failed");
+      else               $display("[BGEU] Testcase (3): Passed");
+      // (6.4) one negative one positive
+      rs1 =-'sd4124;
+      rs2 = 'sd5688;
+      #DELAY;
+      if(branch != 1'b1) $display("[BGEU] Testcase (4): Failed");
+      else               $display("[BGEU] Testcase (4): Passed");
+      // (6.5) two equal numbers
+      rs1 = 'sd12457;
+      rs2 = 'sd12457;
+      #DELAY;
+      if(branch != 1'b1) $display("[BGEU] Testcase (5): Failed");
+      else               $display("[BGEU] Testcase (5): Passed");
+      // (6.6) not equal numbers
+      rs1 = 'sd12457;
+      rs2 =-'sd12457;
+      #DELAY;
+      if(branch != 1'b0) $display("[BGEU] Testcase (6): Failed");
+      else               $display("[BGEU] Testcase (6): Passed");
+      
   end
 
 /******************** Tasks & Functions *******************/
-  task beq_test();
-    begin
-    end
-  endtask
 
 
 /******************** DUT Instantiation *******************/
