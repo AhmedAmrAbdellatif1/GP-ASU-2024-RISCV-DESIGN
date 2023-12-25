d55b6e89a9560634f3bd57302efffe9a6bbb6d42
HebatallahHesham <154348911+HebatallahHesham@users.noreply.github.com>
2023-12-25 21:29:01 +0200

    decode_stage

--- design/decode_stage.sv
+++ design/decode_stage.sv
@@ -24,7 +24,7 @@ assign o_riscv_dstage_rs2addr=i_riscv_dstage_inst[24:20];
 assign o_riscv_dstage_rdaddr=i_riscv_dstage_inst[11:7];
 assign o_riscv_dstage_func7_5= i_riscv_dstage_inst[30];
 assign o_riscv_dstage_funct3= i_riscv_dstage_inst[14:12];
-assign oo_riscv_dstage_opcode= i_riscv_dstage_inst[6:0];
+assign o_riscv_dstage_opcode= i_riscv_dstage_inst[6:0];
 
 
 
