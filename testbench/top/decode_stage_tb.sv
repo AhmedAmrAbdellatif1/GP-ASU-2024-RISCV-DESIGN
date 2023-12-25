6d544c885703c9d517935108c40854b936ab812e
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2023-12-25 19:35:11 +0200

    test text

--- testbench/top/decode_stage_tb.sv
+++ testbench/top/decode_stage_tb.sv
@@ -31,8 +31,60 @@ module riscv_top_tb();
   end
 
 /******************** Tasks & Functions *******************/
+  
+  
+assign rs1addr  = DUT.u_top_datapath.u_riscv_dstage.o_riscv_dstage_rs1addr;  
+task rs1addr_check;
+input [4:0] expect_addr;
+  begin
+    if(rs1addr!= expect_addr) $display("[%0t] rs1 addr failed",$time);
+  end
+endtask
 
 
+assign rs2addr  = DUT.u_top_datapath.u_riscv_dstage.o_riscv_dstage_rs2addr;
+task rs2addr_check;
+  input [4:0] expect_addr;
+  begin
+    if(rs2addr!= expect_addr) $display("[%0t] rs2 addr failed",$time);
+  end
+endtask
+
+
+assign rdaddr   = DUT.u_top_datapath.u_riscv_dstage.o_riscv_dstage_rdaddr;
+task rdaddr_check;
+  input [4:0] expect_addr;
+  begin
+    if(rdaddr!= expect_addr) $display("[%0t] rs1 addr failed",$time);
+  end
+endtask
+
+
+assign rs1data  = DUT.u_top_datapath.u_riscv_dstage.o_riscv_dstage_rs1data;
+task rs1data_check;
+  input [63:0] expect_data;
+  begin
+    if(rs1data!= expect_data) $display("[%0t] rs1 addr failed",$time);
+  end
+endtask
+
+
+assign rs2data  = DUT.u_top_datapath.u_riscv_dstage.o_riscv_dstage_rs2data;  
+task rs2data_check;
+  input [63:0] expect_data;
+  begin
+    if(rs2data!= expect_data) $display("[%0t] rs2 data failed",$time);
+  end
+endtask
+
+
+assign imm  = DUT.u_top_datapath.u_riscv_dstage.o_riscv_dstage_simm;
+task imm_check;
+  input [63:0] expect_imm;
+  begin
+    if(imm!= expect_imm) $display("[%0t] imm failed",$time);
+  end
+endtask
 
 /******************** DUT Instantiation *******************/
 
@@ -41,4 +93,5 @@ module riscv_top_tb();
     .i_riscv_clk(clk),
     .i_riscv_rst(rst)
   );
+
 endmodule
