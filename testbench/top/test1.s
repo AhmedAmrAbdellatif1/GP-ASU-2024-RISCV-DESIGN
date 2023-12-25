fba19607d4f3f16b27ba75c1847c046238e45ee1
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2023-12-25 17:08:27 +0200

    test

--- testbench/top/test1.s
+++ testbench/top/test1.s
@@ -12,30 +12,30 @@ ld x7, 0(x6)             # (0x1c) Load value from address 0 in memory to x7 (I-t
 add x10, x8, x9          # (0x20) Addition  
 lui x5, 0x20000          # (0x24) Load upper immediate (U-type instruction)
 bne x6, x7, -20          # (0x28) Assume Branch not taken (B-type instruction)
-addiw x1, x0, 
-jalr x4, x1, 8           # (0x2c) Jump and Link Register (I-type instruction)
-nop                      # (0x30) No operation (I-type instruction)
-sltu x3, x8, x9          # (0x34) No operation (I-type instruction)
+addiw x1, x0, 18         # (0x2c)
+jalr x4, x1, 8           # (0x30) Jump and Link Register (I-type instruction)
+nop                      # (0x34) No operation (I-type instruction)
+sltu x3, x8, x9          # (0x38) No operation (I-type instruction)
 
 # Hazards !!!
 # case (1)
-addi x8, x0, 15          # (0x38) Initialize x8 with value 15 (R-type instruction)
-sub x27, x8, x9           # (0x3c) [ForwardAE = 10]   x8 before was 10, recently it's 15
-or x29, x9, x8            # (0x40) [ForwardBE = 01]   x8 before was 10, recently it's 15
-slliw x28, x9, 2         # (0x44) No Forwarding
-nop                      # (0x48) No operation (I-type instruction)
+addi x8, x0, 15          # (0x3c) Initialize x8 with value 15 (R-type instruction)
+sub x27, x8, x9           # (0x40) [ForwardAE = 10]   x8 before was 10, recently it's 15
+or x29, x9, x8            # (0x44) [ForwardBE = 01]   x8 before was 10, recently it's 15
+slliw x28, x9, 2         # (0x4c) No Forwarding
+nop                      # (0x50) No operation (I-type instruction)
 
 # case (2)
-addi x9, x0, 30          # (0x48) Initialize x9 with value 30 (R-type instruction)
-add  x0, x9, x9          # (0x4c) No Forwarding
+addi x9, x0, 30          # (0x54) Initialize x9 with value 30 (R-type instruction)
+add  x0, x9, x9          # (0x58) No Forwarding
 
 # case (3)
-lb x7, 0(x6)             # (0x50) Load value from address 0 in memory to x7 (I-type instruction)
-add x4, x0, x7           # (0x54) Stall for one cycle --> PC && F/D pipe  // Flush --> D/E pipe
+lb x7, 0(x6)             # (0x5c) Load value from address 0 in memory to x7 (I-type instruction)
+add x4, x0, x7           # (0x60) Stall for one cycle --> PC && F/D pipe  // Flush --> D/E pipe
 
 # case (4)
-beq x0, x0, 16           # (0x58) Branch Taken : Flush --> F/D && D/E
-addi x1, x2, 124         # (0x5c)
-addi x1, x2, 125         # (0x60)
-addi x1, x2, 127         # (0x64) 
-addi x1, x0, 10          # (0x68) 
+beq x0, x0, 16           # (0x64) Branch Taken : Flush --> F/D && D/E
+addi x1, x2, 124         # (0x68)
+addi x1, x2, 125         # (0x6c)
+addi x1, x2, 127         # (0x70) 
+addi x1, x0, 10          # (0x74) 
