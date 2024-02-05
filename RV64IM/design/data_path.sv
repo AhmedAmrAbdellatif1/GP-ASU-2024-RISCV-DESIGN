a6d9e9b10fc500031b512b44b4095ee61a5dc3b3
HebatallahHesham <154348911+HebatallahHesham@users.noreply.github.com>
2024-02-05 21:50:52 +0200

    updated

--- RV64IM/design/data_path.sv
+++ RV64IM/design/data_path.sv
@@ -1,4 +1,4 @@
-module riscv_datapath_wrong #(parameter width=64) (
+/*module riscv_datapath_wrong #(parameter width=64) (
   input  logic             i_riscv_datapath_clk,
   input  logic             i_riscv_datapath_rst,
   
@@ -298,3 +298,4 @@ module riscv_datapath_wrong #(parameter width=64) (
   );
 
 endmodule
+*/
