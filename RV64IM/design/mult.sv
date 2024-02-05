b3f6c15713d976b38faa9dfe5a918d2dfc621246
HebatallahHesham <154348911+HebatallahHesham@users.noreply.github.com>
2024-02-05 18:06:11 +0200

    multiplier

--- RV64IM/design/mult.sv
+++ RV64IM/design/mult.sv
@@ -4,8 +4,7 @@ input  logic signed [63:0]   rs2,
 input  logic        [2:0]    MULControl,
 output logic signed [63:0]  product
 );
-typedef enum logic [2:0] {mul=3'b100,mulh=3'b101,mulhu=3'b110,mulhsu=3'b111} t_control ;
-t_control MULControl;
+
 logic signed [127:0]  result;
 integer i;
 
@@ -60,18 +59,12 @@ begin
     end
     product=result[127:64];
  end
- default:  begin                               //mul
-    for (i=0; i<64; i=i+1)
-    begin
-        if(rs2[i]==1'b1)
-        result=result+ (rs1<<i);
-        else
-            result=result+128'b0;
-    end
-    product=result[63:0];
+
+ default:  begin     
+    result=0;
+    product=64'b0;
     end
     endcase
-
 
 end 
 endmodule 
