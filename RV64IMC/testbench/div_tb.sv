f9e26d677f02ed33b9813a67db2c79b6509e00ff
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2024-02-13 10:54:09 +0200

    division verified!

--- RV64IMC/testbench/div_tb.sv
+++ RV64IMC/testbench/div_tb.sv
@@ -1,64 +1,89 @@
 `include "pkg.svh"
 
-module mult_tb();
+module div_tb(); 
     longint A;
     longint B;
     longint Y;
-    enum logic [2:0] {off=3'b0xx,mul=3'b100,mulh=3'b101,mulhu=3'b110,mulhsu=3'b111} ctrl_mul;
-    import ranges::*;
+    enum logic [2:0] {off=3'b0xx,div=3'b100,divu=3'b101,rem=3'b110,remu=3'b111} ctrl_div;
 
+    import ranges::*;
+  
     initial
     begin
-        ctrl_mul=mul;
+        ctrl_div=div;
         for (i = 1; i < NO_TESTS+1; i++) begin
             A = $signed($urandom_range(INT64_MIN, INT64_MAX));
             B = $signed($urandom_range(INT64_MIN, INT64_MAX));
-            mul_verify();
+            if( (A==INT64_MIN)&&(B==-'sd1))
+              continue;
+            if( (B=='sd0) )
+              continue; 
+            div_verify();
             #1;
         end
         #5;
         /////////////////////////////////////////////////////////////////////////////
-        ctrl_mul=mulh;
+        ctrl_div=divu;
         for (i = 1; i < NO_TESTS+1; i++) begin
             A = $signed($urandom_range(INT64_MIN, INT64_MAX));
             B = $signed($urandom_range(INT64_MIN, INT64_MAX));
-            mul_verify();
+            if( (A==INT64_MIN)&&(B==-'sd1))
+              continue;
+            if( (B=='sd0) )
+              continue; 
+            div_verify();
             #1;
         end
         #5;
         /////////////////////////////////////////////////////////////////////////////
-        ctrl_mul=mulhu;
+        ctrl_div=rem;
         for (i = 1; i < NO_TESTS+1; i++) begin
             A = $signed($urandom_range(INT64_MIN, INT64_MAX));
             B = $signed($urandom_range(INT64_MIN, INT64_MAX));
-            mul_verify();
+            if( (A==INT64_MIN)&&(B==-'sd1))
+              continue;
+            if( (B=='sd0) )
+              continue; 
+            div_verify();
             #1;
         end
         #5;
         /////////////////////////////////////////////////////////////////////////////
-        ctrl_mul=mulhsu;
+        ctrl_div=remu;
         for (i = 1; i < NO_TESTS+1; i++) begin
             A = $signed($urandom_range(INT64_MIN, INT64_MAX));
             B = $signed($urandom_range(INT64_MIN, INT64_MAX));
-            mul_verify();
+            if( (A==INT64_MIN)&&(B==-'sd1))
+              continue;
+            if( (B=='sd0) )
+              continue; 
+            div_verify();
             #1;
         end
         /////////////////////////////////////////////////////////////////////////////
-        ctrl_mul=off;
+        ctrl_div=off;
         for (i = 1; i < NO_TESTS+1; i++) begin
             A = $signed($urandom_range(INT64_MIN, INT64_MAX));
             B = $signed($urandom_range(INT64_MIN, INT64_MAX));
-            mul_verify();
+            if( (A==INT64_MIN)&&(B==-'sd1))
+              continue;
+            if( (B=='sd0) )
+              continue; 
+            div_verify();
             #1;
         end
-        #1 $stop; 
+        /////////////////////////////////////////////////////////////////////////////
+        div_by_zero();
+        /////////////////////////////////////////////////////////////////////////////
+        overflow();
+        /////////////////////////////////////////////////////////////////////////////
+        #1 $stop;
     end
-
-riscv_multiplier DUT  (
-    .i_riscv_mul_rs1data(A),
-    .i_riscv_mul_rs2data(B),
-    .o_riscv_mul_product(Y),
-    .i_riscv_mul_mulctrl(ctrl_mul)
+riscv_divider DUT  (
+    .i_riscv_div_rs1data(A),
+    .i_riscv_div_rs2data(B),
+    .o_riscv_div_result(Y),
+    .i_riscv_div_divctrl(ctrl_div)
 );
-  `include "mulMethods.sv"
+  `include "divMethods.sv"
 endmodule
---
+++ RV64IMC/testbench/pkg.svh
@@ -0,0 +1,6 @@
+package ranges;
+    longint INT64_MAX = 64'h7FFFFFFFFFFFFFFF;
+    longint INT64_MIN = 64'h8000000000000000;
+    int     NO_TESTS  = 1000;
+    int i;
+endpackage
