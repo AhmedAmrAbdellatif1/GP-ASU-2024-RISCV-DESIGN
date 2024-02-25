88940a208519fd96a27cbbc59d8746f9b1399b7d
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2024-02-25 09:05:37 +0200

    Updating design files for testing load and stores ! :+1:

--- RV64IMC/design/riscv_em_ppreg.sv
+++ RV64IMC/design/riscv_em_ppreg.sv
@@ -1,38 +1,39 @@
   module riscv_em_ppreg(
-    input   logic           i_riscv_em_clk,
-    input   logic           i_riscv_em_rst,
-    input   logic           i_riscv_em_en,
-    input   logic           i_riscv_em_memw_e,
-    input   logic           i_riscv_em_regw_e,
-    input   logic   [1:0]   i_riscv_em_resultsrc_e,
-    input   logic   [1:0]   i_riscv_em_storesrc_e,
-    input   logic   [2:0]   i_riscv_em_memext_e,
-    input   logic   [63:0]  i_riscv_em_pcplus4_e,
-    input   logic   [63:0]  i_riscv_em_result_e,
-    input   logic   [63:0]  i_riscv_em_storedata_e,
-    input   logic   [4:0]   i_riscv_em_rdaddr_e,
-    input   logic   [63:0]  i_riscv_em_imm_e,
-    input   logic   [6:0]   i_riscv_de_opcode_e,
-    output  logic           o_riscv_em_memw_m,
-    output  logic           o_riscv_em_regw_m,
-    output  logic   [1:0]   o_riscv_em_resultsrc_m,
-    output  logic   [1:0]   o_riscv_em_storesrc_m,
-    output  logic   [2:0]   o_riscv_em_memext_m,
-    output  logic   [63:0]  o_riscv_em_pcplus4_m,
-    output  logic   [63:0]  o_riscv_em_result_m,
-    output  logic   [63:0]  o_riscv_em_storedata_m,
-    output  logic   [4:0]   o_riscv_em_rdaddr_m,
-    output  logic   [63:0]  o_riscv_em_imm_m,
-    output  logic   [6:0]   o_riscv_de_opcode_m,
-
-    //------------------------------------>
-    input   logic   [31:0]  i_riscv_em_inst,
-    input   logic   [15:0]  i_riscv_em_cinst,
-    input   logic   [15:0]  i_riscv_em_pc,
-    output  logic   [31:0]  o_riscv_em_inst,
-    output  logic   [15:0]  o_riscv_em_cinst,
-    output  logic   [15:0]  o_riscv_em_pc
-    //<-------------------------------------
+    //---------------------------------------------->
+    `ifdef TEST
+    input   logic   [31:0]  i_riscv_em_inst         ,
+    input   logic   [15:0]  i_riscv_em_cinst        ,
+    input   logic   [63:0]  i_riscv_em_pc           ,
+    output  logic   [31:0]  o_riscv_em_inst         ,
+    output  logic   [15:0]  o_riscv_em_cinst        ,
+    output  logic   [63:0]  o_riscv_em_pc           ,
+    `endif
+    //<----------------------------------------------
+    input   logic           i_riscv_em_clk          ,
+    input   logic           i_riscv_em_rst          ,
+    input   logic           i_riscv_em_en           ,
+    input   logic           i_riscv_em_memw_e       ,
+    input   logic           i_riscv_em_regw_e       ,
+    input   logic   [1:0]   i_riscv_em_resultsrc_e  ,
+    input   logic   [1:0]   i_riscv_em_storesrc_e   ,
+    input   logic   [2:0]   i_riscv_em_memext_e     ,
+    input   logic   [63:0]  i_riscv_em_pcplus4_e    ,
+    input   logic   [63:0]  i_riscv_em_result_e     ,
+    input   logic   [63:0]  i_riscv_em_storedata_e  ,
+    input   logic   [4:0]   i_riscv_em_rdaddr_e     ,
+    input   logic   [63:0]  i_riscv_em_imm_e        ,
+    input   logic   [6:0]   i_riscv_de_opcode_e     ,
+    output  logic           o_riscv_em_memw_m       ,
+    output  logic           o_riscv_em_regw_m       ,
+    output  logic   [1:0]   o_riscv_em_resultsrc_m  ,
+    output  logic   [1:0]   o_riscv_em_storesrc_m   ,
+    output  logic   [2:0]   o_riscv_em_memext_m     ,
+    output  logic   [63:0]  o_riscv_em_pcplus4_m    ,
+    output  logic   [63:0]  o_riscv_em_result_m     ,
+    output  logic   [63:0]  o_riscv_em_storedata_m  ,
+    output  logic   [4:0]   o_riscv_em_rdaddr_m     ,
+    output  logic   [63:0]  o_riscv_em_imm_m        ,
+    output  logic   [6:0]   o_riscv_de_opcode_m
   );
 
 
@@ -52,9 +53,11 @@
             o_riscv_em_imm_m        <= 'b0;            
             o_riscv_de_opcode_m     <= 'b0;
             //---------------------------->
+            `ifdef TEST
             o_riscv_em_inst         <= 'b0;
             o_riscv_em_cinst        <= 'b0;
             o_riscv_em_pc           <= 'b0;
+            `endif
             //<----------------------------
           end
         else
@@ -74,9 +77,11 @@
         o_riscv_em_imm_m        <= o_riscv_em_imm_m;
         o_riscv_de_opcode_m     <= o_riscv_de_opcode_m;
         //---------------------------------------->
+        `ifdef TEST
         o_riscv_em_inst         <= o_riscv_em_inst;
         o_riscv_em_cinst        <= o_riscv_em_cinst;
         o_riscv_em_pc           <= o_riscv_em_pc;
+        `endif
         //<----------------------------------------
 end
      else
@@ -93,9 +98,11 @@ end
             o_riscv_em_imm_m        <= i_riscv_em_imm_e;
             o_riscv_de_opcode_m     <= i_riscv_de_opcode_e;
             //----------------------------------------->
+            `ifdef TEST
             o_riscv_em_inst         <= i_riscv_em_inst;
             o_riscv_em_cinst        <= i_riscv_em_cinst;
             o_riscv_em_pc           <= i_riscv_em_pc;
+            `endif
             //<-----------------------------------------
        end
     end
--- RV64IMC/design/riscv_fd_ppreg.sv
+++ RV64IMC/design/riscv_fd_ppreg.sv
@@ -1,15 +1,19 @@
   module riscv_fd_ppreg(
-    input  logic        i_riscv_fd_clk,
-    input  logic        i_riscv_fd_rst,
-    input  logic        i_riscv_fd_flush,
-    input  logic        i_riscv_fd_en,
-    input  logic [63:0] i_riscv_fd_pc_f,
-    input  logic [31:0] i_riscv_fd_inst_f,
-    input  logic [15:0] i_riscv_fd_cinst_f,
-    input  logic [63:0] i_riscv_fd_pcplus4_f,
-    output logic [63:0] o_riscv_fd_pc_d,
-    output logic [31:0] o_riscv_fd_inst_d,
-    output logic [15:0] o_riscv_fd_cinst_d,
+    //---------------------------------------->
+    `ifdef TEST
+    input  logic [15:0] i_riscv_fd_cinst_f    ,
+    output logic [15:0] o_riscv_fd_cinst_d    ,
+    `endif
+    //<----------------------------------------
+    input  logic        i_riscv_fd_clk        ,
+    input  logic        i_riscv_fd_rst        ,
+    input  logic        i_riscv_fd_flush      ,
+    input  logic        i_riscv_fd_en         ,
+    input  logic [63:0] i_riscv_fd_pc_f       ,
+    input  logic [31:0] i_riscv_fd_inst_f     ,
+    input  logic [63:0] i_riscv_fd_pcplus4_f  ,
+    output logic [63:0] o_riscv_fd_pc_d       ,
+    output logic [31:0] o_riscv_fd_inst_d     ,
     output logic [63:0] o_riscv_fd_pcplus4_d
   );
   
@@ -19,8 +23,12 @@
         begin
           o_riscv_fd_pc_d      <=64'b0;
           o_riscv_fd_inst_d    <=32'b0;
-          o_riscv_fd_pcplus4_d <=64'b0; 
+          o_riscv_fd_pcplus4_d <=64'b0;
+          //-------------------------->
+          `ifdef TEST
           o_riscv_fd_cinst_d   <=  'b0;
+          `endif
+          //<--------------------------
         end
     else
       begin
@@ -28,14 +36,23 @@
         begin
           o_riscv_fd_pc_d      <=64'b0;
           o_riscv_fd_inst_d    <=32'b0;
-          o_riscv_fd_pcplus4_d <=64'b0; 
+          o_riscv_fd_pcplus4_d <=64'b0;
+          //-------------------------->
+          `ifdef TEST 
+          o_riscv_fd_cinst_d    <='b0;
+          `endif
+        //<-------------------------- 
         end
       else if (i_riscv_fd_en)
         begin
          o_riscv_fd_pc_d      <=o_riscv_fd_pc_d;
          o_riscv_fd_inst_d    <=o_riscv_fd_inst_d;
-         o_riscv_fd_pcplus4_d <=o_riscv_fd_pcplus4_d; 
+         o_riscv_fd_pcplus4_d <=o_riscv_fd_pcplus4_d;
+        //-------------------------->
+        `ifdef TEST 
          o_riscv_fd_cinst_d   <=o_riscv_fd_cinst_d;
+        `endif
+        //<--------------------------
         end
           
        else
@@ -43,7 +60,11 @@
           o_riscv_fd_pc_d      <= i_riscv_fd_pc_f;
           o_riscv_fd_inst_d    <= i_riscv_fd_inst_f;
           o_riscv_fd_pcplus4_d <= i_riscv_fd_pcplus4_f;
+          //-------------------------->
+          `ifdef TEST
           o_riscv_fd_cinst_d   <= i_riscv_fd_cinst_f;
+          `endif
+          //<--------------------------
          end 
     end
   end
