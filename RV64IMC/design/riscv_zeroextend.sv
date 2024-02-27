d3c2b25d1fe8e89ee49a47f7ce360bf59eb786dc
Omarezz123 <105800539+Omarezz123@users.noreply.github.com>
2024-02-28 00:09:38 +0200

    riscv_zeroextend.sv

--- RV64IMC/design/riscv_zeroextend.sv
+++ RV64IMC/design/riscv_zeroextend.sv
@@ -1,1 +1,13 @@
+module riscv_zeroextend
+ (
+   input  logic  [4:0]  i_riscv_zeroextend_imm ,
+   output logic  [63:0] o_riscv_zeroextend_immextend
+ ) ;
 
+  always @(*) begin 
+  	
+  	o_riscv_zeroextend_immextend = { {59{1'b0}} , i_riscv_zeroextend_imm } ;
+                                   
+  end
+ 
+ endmodule 
