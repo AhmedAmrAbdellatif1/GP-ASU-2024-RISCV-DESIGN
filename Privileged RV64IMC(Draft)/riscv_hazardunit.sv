cd387da3d0af653eb6739ffa4a27087ca43d01d5
Omarezz123 <105800539+Omarezz123@users.noreply.github.com>
2024-03-02 22:25:01 +0200

    Update riscv_hazardunit.sv

--- Privileged RV64IMC(Draft)/riscv_hazardunit.sv
+++ Privileged RV64IMC(Draft)/riscv_hazardunit.sv
@@ -18,6 +18,7 @@ module riscv_hazardunit (
    input  logic           i_riscv_hzrdu_iscsr_e        ,   // for csr
    input  logic           i_riscv_hzrdu_iscsr_d       ,   // for csr
    input  logic           i_riscv_hzrdu_iscsr_w        ,   // for csr
+   input logic            i_riscv_hzrdu_iscsr_m       ,
    input  logic            o_riscv_hzrdu_passwb        ,
    output logic   [1:0]   o_riscv_hzrdu_fwda          , 
    output logic   [1:0]   o_riscv_hzrdu_fwdb          , 
@@ -131,7 +132,7 @@ always_comb
 
   always @(*)
     begin
-      if  ( i_riscv_hzrdu_iscsr_e == 1'b1 &&  i_riscv_hzrdu_iscsr_w == 1'b1 )
+      if  ( i_riscv_hzrdu_iscsr_m == 1'b1 &&  i_riscv_hzrdu_iscsr_w == 1'b1 )
           o_riscv_hzrdu_passwb = 1 ;
       else
           o_riscv_hzrdu_passwb = 0 ;        
