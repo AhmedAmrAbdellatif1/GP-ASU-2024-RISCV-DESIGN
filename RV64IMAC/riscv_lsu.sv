6201b62efa3a977b5d902f4444a8c8e760a9d461
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2024-03-22 14:58:44 +0200

    Update riscv_lsu.sv

--- RV64IMAC/riscv_lsu.sv
+++ RV64IMAC/riscv_lsu.sv
@@ -1,20 +1,20 @@
 module riscv_lsu (
-    input  logic            i_riscv_lsu_clk,         
-    input  logic            i_riscv_lsu_rst,                
+    input  logic            i_riscv_lsu_clk,
+    input  logic            i_riscv_lsu_rst,
     input  logic  [63:0]    i_riscv_lsu_address,           //rs1
     input  logic  [63:0]    i_riscv_lsu_alu_result,
     input  logic  [1:0]     i_riscv_lsu_LR,                // [1] bit indicates LR or not, [0] indicates word or double word
     input  logic  [1:0]     i_riscv_lsu_SC,                // [1] bit indicates SC or not, [0] indicates word or double word
-    input  logic            i_riscv_lsu_AMO,                    
+    input  logic            i_riscv_lsu_AMO,
     input  logic            i_riscv_lsu_memwrite,
     input  logic            i_riscv_lsu_memread,
     input  logic            i_riscv_lsu_goto_trap,         //output of CSR
     input  logic            i_riscv_lsu_return_trap,       //output of CSR
-    input  logic            i_riscv_lsu_misalignment,       
-    output logic            o_riscv_lsu_memwrite_en,        
-    output logic            o_riscv_lsu_memread_en,         
-    output logic  [63:0]    o_riscv_lsu_mem_address,      
-    output logic            o_riscv_lsu_sc_rdvalue          
+    input  logic            i_riscv_lsu_misalignment,
+    output logic            o_riscv_lsu_memwrite_en,
+    output logic            o_riscv_lsu_memread_en,
+    output logic  [63:0]    o_riscv_lsu_mem_address,
+    output logic            o_riscv_lsu_sc_rdvalue
   );
   typedef enum logic [4:0] {
             NORMAL_READ  = 5'b10000,
@@ -99,60 +99,56 @@ module riscv_lsu (
       o_riscv_lsu_sc_rdvalue = 'b0  ;
       //  o_riscv_lsu_memwrite_en = i_riscv_lsu_memwrite;
     end
-
-    always_comb
-    begin
-      case(sel)
-
-        NORMAL_READ:
-        begin
-          o_riscv_lsu_memread_en= i_riscv_lsu_memread && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
-          o_riscv_lsu_memwrite_en= 0;
-          o_riscv_lsu_mem_address=i_riscv_lsu_alu_result;
-        end
-
-        NORMAL_WRITE:
-        begin
-          o_riscv_lsu_memread_en= 0;
-          o_riscv_lsu_memwrite_en=i_riscv_lsu_memwrite && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
-          o_riscv_lsu_mem_address=i_riscv_lsu_alu_result;
-        end
-
-        LR :
-        begin
-          o_riscv_lsu_memread_en= i_riscv_lsu_LR[1]  && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
-          o_riscv_lsu_memwrite_en= 0;
-          o_riscv_lsu_mem_address=i_riscv_lsu_address;
-        end
-
-        SC :
-        begin
-          o_riscv_lsu_memread_en= 0;
-          o_riscv_lsu_mem_address=i_riscv_lsu_address;
-          if((i_riscv_lsu_address ==  reserv_addr) && reserv_valid && (LR_word == i_riscv_lsu_SC [0]) &&i_riscv_lsu_SC[1] && !i_riscv_lsu_goto_trap  && !i_riscv_lsu_return_trap)
-            o_riscv_lsu_memwrite_en= 1;
-          else
-            o_riscv_lsu_memwrite_en= 0;
-        end
-
-        AMO:
-        begin
-          o_riscv_lsu_memread_en= i_riscv_lsu_memread && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
-          o_riscv_lsu_memwrite_en= 0;
-          o_riscv_lsu_mem_address=i_riscv_lsu_address;
-        end
-
-        default:
-        begin
-          o_riscv_lsu_memread_en  = 1'b0;
-          o_riscv_lsu_memwrite_en = 1'b0;
-          o_riscv_lsu_mem_address =  'b0;
-        end
-
-
-      endcase
-    end
   end
 
+  always_comb
+  begin
+    case(sel)
+      NORMAL_READ:
+      begin
+        o_riscv_lsu_memread_en= i_riscv_lsu_memread && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
+        o_riscv_lsu_memwrite_en= 0;
+        o_riscv_lsu_mem_address=i_riscv_lsu_alu_result;
+      end
+
+      NORMAL_WRITE:
+      begin
+        o_riscv_lsu_memread_en= 0;
+        o_riscv_lsu_memwrite_en=i_riscv_lsu_memwrite && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
+        o_riscv_lsu_mem_address=i_riscv_lsu_alu_result;
+      end
+
+      LR :
+      begin
+        o_riscv_lsu_memread_en= i_riscv_lsu_LR[1]  && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
+        o_riscv_lsu_memwrite_en= 0;
+        o_riscv_lsu_mem_address=i_riscv_lsu_address;
+      end
+
+      SC :
+      begin
+        o_riscv_lsu_memread_en= 0;
+        o_riscv_lsu_mem_address=i_riscv_lsu_address;
+        if((i_riscv_lsu_address ==  reserv_addr) && reserv_valid && (LR_word == i_riscv_lsu_SC [0]) &&i_riscv_lsu_SC[1] && !i_riscv_lsu_goto_trap  && !i_riscv_lsu_return_trap)
+          o_riscv_lsu_memwrite_en= 1;
+        else
+          o_riscv_lsu_memwrite_en= 0;
+      end
+
+      AMO:
+      begin
+        o_riscv_lsu_memread_en= i_riscv_lsu_memread && !i_riscv_lsu_goto_trap && !i_riscv_lsu_return_trap;
+        o_riscv_lsu_memwrite_en= 0;
+        o_riscv_lsu_mem_address=i_riscv_lsu_address;
+      end
+
+      default:
+      begin
+        o_riscv_lsu_memread_en  = 1'b0;
+        o_riscv_lsu_memwrite_en = 1'b0;
+        o_riscv_lsu_mem_address =  'b0;
+      end
+    endcase
+  end
 
 endmodule
