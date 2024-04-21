39cd751234929b361c125704cb509e2e7c180e70
Omarezz123 <105800539+Omarezz123@users.noreply.github.com>
2024-04-21 20:36:16 +0200

    Update riscv_wb_stage.sv

--- S Mode/riscv_wb_stage.sv
+++ S Mode/riscv_wb_stage.sv
@@ -10,6 +10,7 @@ module riscv_wbstage (
    input    logic [1:0]    i_riscv_wb_returnfromtrap  , //<--- Trap
    input    logic          i_riscv_wb_icache_stall    ,
    input    logic [63:0]   i_riscv_wb_rddata_sc       ,
+   input    logic          i_riscv_wb_reconfig        ,
    output   logic [1:0]    o_riscv_wb_pcsel           , //<--- Trap
    output   logic          o_riscv_wb_flush           , //<--- Trap
    output   logic [63:0]   o_riscv_wb_rddata            //modified position
@@ -27,10 +28,12 @@ riscv_mux5 u_result_mux (
    .o_riscv_mux5_out  (riscv_wb_rddata)
 );
 
+
 riscv_trap_wb trap_wb (
   .i_riscv_trap_gototrap        (i_riscv_wb_gototrap)         ,                  
   .i_riscv_trap_returnfromtrap  (i_riscv_wb_returnfromtrap)   ,
   .i_riscv_trap_icache_stall    (i_riscv_wb_icache_stall)     ,
+  .i_riscv_trap_reconfig_pip    (i_riscv_wb_reconfig)         ,
   .o_riscv_trap_flush           (o_riscv_wb_flush)            ,                    
   .o_riscv_trap_pcsel           (o_riscv_wb_pcsel)                       
 );
