528b2a11e59aa3889241097afed930970cbfb1fc
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2024-04-26 03:13:08 +0300

    UPDATE

--- S Mode/riscv_tracer.sv
+++ S Mode/riscv_tracer.sv
@@ -1,22 +1,30 @@
 module riscv_tracer #(parameter dwidth=64,iwidth=32,awidth=5,cwidth=16) (
-  input  logic                i_riscv_clk         ,
-  input  logic                i_riscv_rst         ,
-  input  logic [iwidth-1:0]   i_riscv_trc_inst    ,
-  input  logic [cwidth-1:0]   i_riscv_trc_cinst   ,
-  input  logic [awidth-1:0]   i_riscv_trc_rdaddr  ,
-  input  logic [dwidth-1:0]   i_riscv_trc_memaddr ,
-  input  logic [dwidth-1:0]   i_riscv_trc_pc      ,
-  input  logic [dwidth-1:0]   i_riscv_trc_store   ,
-  input  logic [dwidth-1:0]   i_riscv_trc_rddata  );
+  input   logic                i_riscv_clk         ,
+  input   logic                i_riscv_rst         ,
+  input   logic [iwidth-1:0]   i_riscv_trc_inst    ,
+  input   logic [cwidth-1:0]   i_riscv_trc_cinst   ,
+  input   logic [awidth-1:0]   i_riscv_trc_rdaddr  ,
+  input   logic [dwidth-1:0]   i_riscv_trc_memaddr ,
+  input   logic [dwidth-1:0]   i_riscv_trc_pc      ,
+  input   logic [dwidth-1:0]   i_riscv_trc_store   ,
+  input   logic [dwidth-1:0]   i_riscv_trc_rddata  ,
+  
+  output  logic [iwidth-1:0]   o_riscv_trc_inst    ,
+  output  logic [cwidth-1:0]   o_riscv_trc_cinst   ,
+  output  logic [awidth-1:0]   o_riscv_trc_rdaddr  ,
+  output  logic [dwidth-1:0]   o_riscv_trc_memaddr ,
+  output  logic [dwidth-1:0]   o_riscv_trc_pc      ,
+  output  logic [dwidth-1:0]   o_riscv_trc_store   ,
+  output  logic [dwidth-1:0]   o_riscv_trc_rddata  );
 
   // internal registers
-  logic [iwidth-1:0]   reg_riscv_trc_inst     ;
-  logic [cwidth-1:0]   reg_riscv_trc_cinst    ;
-  logic [awidth-1:0]   reg_riscv_trc_rdaddr   ;
-  logic [dwidth-1:0]   reg_riscv_trc_memaddr  ;
-  logic [dwidth-1:0]   reg_riscv_trc_pc       ;
-  logic [dwidth-1:0]   reg_riscv_trc_store    ;
-  logic [dwidth-1:0]   reg_riscv_trc_rddata   ;
+  (* dont_touch = “yes” *) logic [iwidth-1:0]   reg_riscv_trc_inst     ;
+  (* dont_touch = “yes” *) logic [cwidth-1:0]   reg_riscv_trc_cinst    ;
+  (* dont_touch = “yes” *) logic [awidth-1:0]   reg_riscv_trc_rdaddr   ;
+  (* dont_touch = “yes” *) logic [dwidth-1:0]   reg_riscv_trc_memaddr  ;
+  (* dont_touch = “yes” *) logic [dwidth-1:0]   reg_riscv_trc_pc       ;
+  (* dont_touch = “yes” *) logic [dwidth-1:0]   reg_riscv_trc_store    ;
+  (* dont_touch = “yes” *) logic [dwidth-1:0]   reg_riscv_trc_rddata   ;
 
 
  always_ff @(posedge i_riscv_clk or posedge i_riscv_rst )
@@ -43,4 +51,14 @@ module riscv_tracer #(parameter dwidth=64,iwidth=32,awidth=5,cwidth=16) (
          reg_riscv_trc_rddata   <= i_riscv_trc_rddata;
         end
     end
+
+    assign o_riscv_trc_inst     = i_riscv_trc_inst    ;
+    assign o_riscv_trc_cinst    = i_riscv_trc_cinst   ;
+    assign o_riscv_trc_rdaddr   = i_riscv_trc_rdaddr  ;
+    assign o_riscv_trc_memaddr  = i_riscv_trc_memaddr ;
+    assign o_riscv_trc_pc       = i_riscv_trc_pc      ;
+    assign o_riscv_trc_store    = i_riscv_trc_store   ;
+    assign o_riscv_trc_rddata   = i_riscv_trc_rddata  ;
+
+
 endmodule 
--- S Mode/riscv_zeroextend.sv
+++ S Mode/riscv_zeroextend.sv
@@ -1,8 +1,9 @@
 module riscv_zeroextend (
-  input  logic  [4:0]   i_riscv_zeroextend_imm      ,
-  output logic  [63:0]  o_riscv_zeroextend_immextend
- );
-  always_comb begin
+    input  logic  [4:0]   i_riscv_zeroextend_imm        ,
+    output logic  [63:0]  o_riscv_zeroextend_immextend
+  );
+  always_comb 
+  begin
     o_riscv_zeroextend_immextend = { {59{1'b0}} , i_riscv_zeroextend_imm } ;
   end
  endmodule 
